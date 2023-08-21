1d22c6d053214217b01b6c73d15915582bfc5764
Maxim Antipin <max.v.antipin@gmail.com>
2023-08-22 02:56:26 +0300

    Add class MaxieSystems\URL\DomainName\Labels

--- src/URL/DomainName/Labels.php
+++ src/URL/DomainName/Labels.php
@@ -2,148 +2,76 @@
 
 namespace MaxieSystems\URL\DomainName;
 
+use MaxieSystems\Exception\Messages as EMsg;
+
 class Labels implements \ArrayAccess, \Countable
 {
-    final public static function Check(string $label): string
+    final public function __construct(string $value)
     {
-        if ('' === $label) {
-            throw new \UnexpectedValueException('Domain label is empty');
-        } elseif ('.' === $label) {
-            throw new \UnexpectedValueException('Domain label is invalid: ' . var_export($label, true));
-        }
-        return $label;
-    }
-
-    final public function __construct(string $v)
-    {
-        if ('' === $v) {
-            $this->value = [];
+        if ('' === $value) {
+            $this->labels = [];
         } else {
-            $this->value = explode('.', $v);
-            if ('' === end($this->value)) {
-                array_pop($this->value);
+            $value = explode('.', $value);
+            if ('' === end($value)) {
+                array_pop($value);
             }
+            $this->labels = $value;
         }
     }
 
-    final public function offsetGet($i)
+    final public function offsetGet($i): ?string
     {
         if (is_int($i)) {
-            $j = $this->ConvertOffset($i);
-            return isset($this->value[$j]) ? $this->value[$j] : null;# Решено не выбрасывать исключение при отсутствующем числовом индексе; предполагался вызов E\UndefinedIndex().
+            $j = $this->convertOffset($i);
+            return $this->labels[$j] ?? null;
         }
-        list($e, $m, $c) = E\InvalidIndex($i);
-        throw new $e($m, $c);
+        throw new \TypeError(EMsg::illegalOffsetType($i));
     }
 
-// специальный метод(ы) с проверками: Insert(?int $index, $) или Replace ??? Add ???
-// если передаётся объект, массив, другое - проверять типы?
-// если я могу удалять только последний элемент (и не могу остальные произвольно), то почему можно произвольно задавать любой элемент таким вызовом $labels[2] = 'my-site'; или даже $labels[1] = 'co.uk'?
-// А как заменить co.uk на com?
-// Насколько интуитивно понятна запись $labels[] = 'static-5'???
-    // final public function offsetSet($i, $v)
-     // {//throw new \TypeError('Argument 1 passed to '.__METHOD__.'() must be iterable or be an instance of stdClass or be of the type string, '.gettype($q).' given');
-        // if($v instanceof namespace\DomainNameLabels)throw new \Exception('not implemented yet...');
-        // else
-         // {
-            // $v = explode('.', "$v");# Вызов explode('.', '') вернёт такой массив: array(1) { [0]=> string(0) "" }
-            // foreach($v as $j) self::Check($j);
-         // }
-        // if(null === $i)
-         // {
-            // array_unshift($this->value, ...$v);
-            // return;
-         // }
-        // elseif(is_int($i))
-         // {
-            // $j = $this->ConvertOffset($i);
-            // if(isset($this->value[$j]))
-             // {throw new \Exception('not implemented yet...');
-                // if(count($v) > 1) array_splice($this->value, $j, 1, $v);
-                // else $this->value[$j] = $v[0];
-                // return;
-             // }
-            // else list($e, $m, $c) = E\UndefinedIndex($i);
-         // }
-        // else list($e, $m, $c) = E\InvalidIndex($i);
-        // throw new $e($m, $c);
-     // }
-
-    final public function offsetSet($i, $v)
+    final public function offsetSet($i, $v): void
     {
-        if (null === $i) {
-            array_unshift($this->value, $v);
-            return;
-        } elseif (is_int($i)) {
-            $j = $this->ConvertOffset($i);
-            if (isset($this->value[$j])) {
-                $this->value[$j] = $v;
-                return;
-            } else {
-                list($e, $m, $c) = E\UndefinedIndex($i);
-            }
-        } else {
-            list($e, $m, $c) = E\InvalidIndex($i);
-        }
-        throw new $e($m, $c);
+        throw new \Error(EMsg::readonlyObject($this));
     }
 
-    final public function offsetUnset($i)
+    final public function offsetUnset($i): void
     {
-        if (is_int($i)) {
-            $j = $this->ConvertOffset($i);
-            if (isset($this->value[$j])) {
-                if (0 === $j) {
-                    array_shift($this->value);# Удаление последнего элемента: индекс равен count() или -1; например, для www.example.com будет удалено www.
-                } else {
-                    throw new \Exception('not implemented yet...');//Пока не решено, что делать, поскольку решение "в лоб" не подходит: нет смысла в удалении example или com из www.example.com.
-                }
-            }
-        } else {
-            list($e, $m, $c) = E\InvalidIndex($i);
-            throw new $e($m, $c);
-        }
+        throw new \Error(EMsg::readonlyObject($this));
     }
 
-    final public function Exists(int $i, int &$j = null): bool
-    {
-        $j = $this->ConvertOffset($i);// $j должно быть равно чему? по идее, индексу от 1 (внешнему), а не от 0 (внутреннему).
-        if (isset($this->value[$j])) {
-            throw new \Exception('not implemented yet...');
-        } else {
-            throw new \Exception('not implemented yet...');
-        }
-    }
-
-    final public function ToArray(): array
+    final public function toArray(): array
     {
         $r = [];
-        $cnt = count($this->value);
+        $cnt = count($this->labels);
         for ($i = $cnt; $i > 0; --$i) {
-            $r[$i] = $this->value[$cnt - $i];
+            $r[$i] = $this->labels[$cnt - $i];
         }
         return $r;
     }
 
     final public function __debugInfo(): array
     {
-        return $this->ToArray();
-    }
-    final public function offsetExists($i)
-    {
-        return is_int($i) ? isset($this->value[$this->ConvertOffset($i)]) : false;
-    }
-    final public function __toString()
-    {
-        return implode('.', $this->value);
-    }
-    final public function count()
-    {
-        return count($this->value);
+        return $this->toArray();
     }
 
-    final protected function ConvertOffset(int $k): int
+    final public function offsetExists($i): bool
     {
-        return $k < 0 ? -1 - $k : count($this->value) - $k;
+        return is_int($i) ? isset($this->labels[$this->convertOffset($i)]) : false;
     }
+
+    final public function __toString()
+    {
+        return implode('.', $this->labels);
+    }
+
+    final public function count(): int
+    {
+        return count($this->labels);
+    }
+
+    final protected function convertOffset(int $k): int
+    {
+        return $k < 0 ? -1 - $k : count($this->labels) - $k;
+    }
+
+    private readonly array $labels;
 }
---
+++ tests/URL/DomainName/LabelsTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaxieSystems\URL\DomainName;
+
+use PHPUnit\Framework\TestCase;
+
+final class LabelsTest extends TestCase
+{
+    public function testToString(): void
+    {
+        $domain = 'www.example.com';
+        $labels = new Labels($domain);
+        $this->assertSame($domain, (string)$labels);
+        $labels = new Labels($domain . '.');
+        $this->assertSame($domain, (string)$labels);
+    }
+
+    public function testCount(): void
+    {
+        $domain = 'www.example.com';
+        $labels = new Labels($domain);
+        $this->assertCount(3, $labels);
+        $domain = 'localhost';
+        $labels = new Labels($domain);
+        $this->assertCount(1, $labels);
+        $domain = 'static.example.co.uk.';
+        $labels = new Labels($domain);
+        $this->assertCount(4, $labels);
+    }
+
+    public function testOffsetIsset(): void
+    {
+        $domain = 'www.example.com';
+        $labels = new Labels($domain);
+        foreach ([1, 2, -1] as $i) {
+            $this->assertTrue(isset($labels[$i]));
+        }
+        foreach ([11, '2', -11, 'www'] as $i) {
+            $this->assertNotTrue(isset($labels[$i]));
+        }
+    }
+
+    public function testOffsetGet(): void
+    {
+        $domain = 'www.example.com';
+        $labels = new Labels($domain);
+        $this->assertSame('example', $labels[2]);
+        $this->assertSame('www', $labels[-1]);
+        $this->expectException(\TypeError::class);
+        echo $labels['22'];
+    }
+
+    public function testOffsetSet(): void
+    {
+        $domain = 'www.example.com';
+        $labels = new Labels($domain);
+        $this->expectException(\Error::class);
+        $labels[2] = 'xxx';
+    }
+
+    public function testOffsetUnset(): void
+    {
+        $domain = 'www.example.com';
+        $labels = new Labels($domain);
+        $this->expectException(\Error::class);
+        unset($labels[2]);
+    }
+
+    public function testToArray(): void
+    {
+        $domain = 'www.example.com';
+        $labels = new Labels($domain);
+        $arr = $labels->toArray();
+        $this->assertArrayHasKey(3, $arr);
+        $this->assertArrayHasKey(2, $arr);
+        $this->assertArrayHasKey(1, $arr);
+        $this->assertSame('www', $arr[3]);
+        $this->assertSame('example', $arr[2]);
+        $this->assertSame('com', $arr[1]);
+    }
+}
