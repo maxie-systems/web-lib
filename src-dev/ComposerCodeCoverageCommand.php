ed4af3639106f182fb9d94a681419f4d2e867689
Maxim Antipin <max.v.antipin@gmail.com>
2023-12-29 18:49:16 +0300

    Remove unused code fragment

--- src-dev/ComposerCodeCoverageCommand.php
+++ src-dev/ComposerCodeCoverageCommand.php
@@ -5,7 +5,6 @@ namespace MaxieSystems\Dev;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Exception\RuntimeException;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
