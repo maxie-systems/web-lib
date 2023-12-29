c4420c133b447737efd1006232fc9a491c10e093
Maxim Antipin <max.v.antipin@gmail.com>
2023-12-29 18:24:03 +0300

    Split GitHub Actions step into 3 steps

--- src-dev/ComposerCodeCoverageCommand.php
+++ src-dev/ComposerCodeCoverageCommand.php
@@ -4,6 +4,7 @@ namespace MaxieSystems\Dev;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidOptionException;
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -32,11 +33,6 @@ class ComposerCodeCoverageCommand extends Command
                 InputOption::VALUE_NONE,
                 'Do not fail the action when the minimum coverage was not met.'
             ),
-            new InputArgument(
-                'path',
-                InputArgument::REQUIRED,
-                'The path to the report file.'
-            ),
         ]);
     }
 
@@ -46,19 +42,29 @@ class ComposerCodeCoverageCommand extends Command
         if ($min_coverage < 50 || $min_coverage > 100) {
             throw new InvalidOptionException('Minimum coverage must be between 50 and 100');
         }
+        $doc = new \DOMDocument();
+        $doc->load('./phpunit.xml');
+        $xpath = new \DOMXPath($doc);
+        $items = $xpath->query('coverage/report/text');
+        if (!$items->length) {
+            throw new RuntimeException('Unable to find report file name');
+        }
         $below_threshold = false;
         $output->writeln('Code Coverage Report Summary:');
-        $text = file_get_contents($input->getArgument('path'));
+        $output->writeln(" Minimum allowed coverage is $min_coverage%");
+        $text = file_get_contents($items[0]->getAttribute('outputFile'));
         $rx = '\\s+(?P<rate>[0-9]{1,3}(\\.[0-9]+)?)% \((?P<covered>[0-9]+)\/(?P<valid>[0-9]+)\)';
         foreach (['Classes', 'Methods', 'Paths', 'Branches', 'Lines'] as $label) {
             if (preg_match("/$label:$rx/", $text, $m)) {
-                $output->writeln($this->alignLabelsAndNumbers($label, $m['rate'], $m['covered'], $m['valid']));
+                $s = self::TAB . $this->alignLabelsAndNumbers($label, $m['rate'], $m['covered'], $m['valid']);
                 if ($m['rate'] < $min_coverage) {
                     $below_threshold = true;
+                    $s = "<error>$s </error>";
                 }
+                $output->writeln($s);
             }
         }
-        $output->writeln(" Minimum allowed coverage is $min_coverage%");
+        $output->writeln('');
         if ($input->getOption('ignore-threshold')) {
             return self::SUCCESS;
         }
@@ -72,7 +78,7 @@ class ComposerCodeCoverageCommand extends Command
         if ($spaces < 1) {
             $spaces = 1;
         }
-        return self::TAB . $label . ':' . str_repeat(' ', $spaces) . $percent . "% ($covered/$valid)";
+        return $label . ':' . str_repeat(' ', $spaces) . $percent . "% ($covered/$valid)";
     }
 
     private const TAB = '  ';
