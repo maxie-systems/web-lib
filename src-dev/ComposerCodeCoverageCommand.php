ebb5539ef602582f7cabe8882aba2280eb028eef
Max Antipin <92847830+max-antipin@users.noreply.github.com>
2024-01-06 02:43:52 +0300

    Create CI pipeline (#3) (#5)
    * Create phpunit.yml to run PHPUnit tests (#3)
    
    * Create phpunit.yml to run PHPUnit tests
    
    * Phpunit GitHub actions (#4)
    
    * Create phpunit.yml to run PHPUnit test on pull request
    * Add code coverage check and summary
    
    * Add push trigger
    
    * Remove tests/integration path from phpcs paths
    
    * Ignore code coverage threshold
    
    * Split GitHub Actions step into 3 steps
    
    * Fix phpunit command
    
    * Remove tests/integration/ directory from testsuite
    
    * Fix code coverage report command
    
    * Remove unused code fragment
    
    * Rename GitHub Actions workflow file
    
    * Add GitHub Actions workflow_dispatch event and update steps
    
    * Change code coverage report directory from 'tests' to 'var'
    
    * Add temporary folder creation inside the CI pipeline

---
+++ src-dev/ComposerCodeCoverageCommand.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MaxieSystems\Dev;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidOptionException;
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class for generating code coverage report.
+ *
+ * @author Max Antipin <max.v.antipin@gmail.com>
+ */
+class ComposerCodeCoverageCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setDefinition([
+            new InputOption(
+                'minimum-coverage',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'The minimum allowed coverage percentage as an integer.',
+                75
+            ),
+            new InputOption(
+                'ignore-threshold',
+                null,
+                InputOption::VALUE_NONE,
+                'Do not fail the action when the minimum coverage was not met.'
+            ),
+        ]);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $min_coverage = (int)$input->getOption('minimum-coverage');
+        if ($min_coverage < 50 || $min_coverage > 100) {
+            throw new InvalidOptionException('Minimum coverage must be between 50 and 100');
+        }
+        $doc = new \DOMDocument();
+        $doc->load('./phpunit.xml');
+        $xpath = new \DOMXPath($doc);
+        $items = $xpath->query('coverage/report/text');
+        if (!$items->length) {
+            throw new RuntimeException('Unable to find report file name');
+        }
+        $below_threshold = false;
+        $output->writeln('Code Coverage Report Summary:');
+        $output->writeln(" Minimum allowed coverage is $min_coverage%");
+        $text = file_get_contents($items[0]->getAttribute('outputFile'));
+        $rx = '\\s+(?P<rate>[0-9]{1,3}(\\.[0-9]+)?)% \((?P<covered>[0-9]+)\/(?P<valid>[0-9]+)\)';
+        foreach (['Classes', 'Methods', 'Paths', 'Branches', 'Lines'] as $label) {
+            if (preg_match("/$label:$rx/", $text, $m)) {
+                $s = self::TAB . $this->alignLabelsAndNumbers($label, $m['rate'], $m['covered'], $m['valid']);
+                if ($m['rate'] < $min_coverage) {
+                    $below_threshold = true;
+                    $s = "<error>$s </error>";
+                }
+                $output->writeln($s);
+            }
+        }
+        $output->writeln('');
+        if ($input->getOption('ignore-threshold')) {
+            return self::SUCCESS;
+        }
+        return $below_threshold ? self::FAILURE : self::SUCCESS;
+    }
+
+    private function alignLabelsAndNumbers(string $label, string $percent, int $covered, int $valid): string
+    {
+        $width = 14;
+        $spaces = $width - strlen($label) - strlen($percent);
+        if ($spaces < 1) {
+            $spaces = 1;
+        }
+        return $label . ':' . str_repeat(' ', $spaces) . $percent . "% ($covered/$valid)";
+    }
+
+    private const TAB = '  ';
+}
