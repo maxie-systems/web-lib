dcdb2414237f41582ff0d495ac37da92499c932c
Maxim Antipin <max.v.antipin@gmail.com>
2024-07-16 05:18:39 +0300

    Add separate service for linting

--- .docker/run-tests.sh
+++ .docker/run-tests.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 set -u
 
-echo 'Run phpcs'
-php ./vendor/bin/phpcs
-
-echo 'Run phpunit'
-if [ "$1" = 'no-coverage' ]; then
-  php ./vendor/bin/phpunit --no-coverage
-elif [ "$1" = 'coverage' ]; then
-  php ./vendor/bin/phpunit --coverage-text --only-summary-for-coverage-text
-else
-  echo "Invalid argument: $1"
+if [[ $# -eq 0 ]]; then
+  echo "No arguments specified!"
   exit 1
 fi
 
-echo 'Run phpstan'
-php ./vendor/bin/phpstan analyse
+for arg in "$@"
+do
+  if [[ $arg = 'phpcs' ]]; then
+    echo 'Run phpcs'
+    php ./vendor/bin/phpcs
+  elif [[ $arg = 'phpstan' ]]; then
+    echo 'Run phpstan'
+    php ./vendor/bin/phpstan analyse
+  elif [[ $arg = 'phpunit-no-coverage' ]]; then
+    echo 'Run phpunit without coverage'
+    php ./vendor/bin/phpunit --no-coverage
+  elif [[ $arg = 'phpunit' ]]; then
+    echo 'Run phpunit with coverage'
+    php ./vendor/bin/phpunit --coverage-text --only-summary-for-coverage-text
+  else
+    echo "Invalid argument: $arg"
+    exit 1
+  fi
+done
